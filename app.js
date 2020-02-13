8a7c81d3b330eae3c39508186310616805c179a3
김슬아 <urghb12375@gmail.com>
2020-02-14 01:22:17 +0900

    20/02/14

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -11,12 +12,14 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
+//helmet은 보안을 위한 것
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
-//helmet은 보안을 위한 것
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
