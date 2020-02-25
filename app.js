c89a4b282e1fd1f87b8777bfe23b52fb8ca37645
김슬아 <urghb12375@gmail.com>
2020-02-26 02:20:55 +0900

    [header scss]

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(helmet());
 // helmet은 보안을 위한 것
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
